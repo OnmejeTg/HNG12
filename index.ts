d7fb784270935f33220545042c72c3178a0ae4fe
onmejetg <tonmeje@gmail.com>
2025-01-30 11:47:11 +0100

    corrected github url

--- index.ts
+++ index.ts
@@ -8,14 +8,14 @@ app.use(cors());
 interface ApiResponse {
   email: string;
   current_datetime: string;
-  github_repo_url: string;
+  github_url: string;
 }
 
 app.get("/hng", (req: Request, res: Response) => {
   const response: ApiResponse = {
     email: "tonmeje@gmail.com",
     current_datetime: new Date().toISOString(),
-    github_repo_url: "https://github.com/OnmejeTg/HNG",
+    github_url: "https://github.com/OnmejeTg/HNG",
   };
   res.json(response);
 });
