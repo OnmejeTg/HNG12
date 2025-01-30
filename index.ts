cafcdcf489d5e37799634f3476ebba7e328d4400
onmejetg <tonmeje@gmail.com>
2025-01-30 05:37:33 +0100

    stage-01

---
+++ index.ts
@@ -0,0 +1,25 @@
+import express, { Express, Request, Response } from "express";
+import cors from "cors";
+
+const app: Express = express();
+
+app.use(cors());
+
+interface ApiResponse {
+  email: string;
+  current_datetime: string;
+  github_repo_url: string;
+}
+
+app.get("/", (req: Request, res: Response) => {
+  const response: ApiResponse = {
+    email: "tonmeje@gmail.com",
+    current_datetime: new Date().toISOString(),
+    github_repo_url: "https://github.com/OnmejeTg/HNG",
+  };
+  res.json(response);
+});
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
